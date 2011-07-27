a192e5dc792185f6340824ac2d21c111e26cf569
Andreas Knab <knab2ar@Andreas-Knabs-MacBook-Pro.local>
2011-07-27 11:31:39 -0700

    Optimized performance and cleaned up code

--- words.py
+++ words.py
@@ -1,7 +1,6 @@
 from __future__ import with_statement
 import itertools
-import datetime
-from copy import copy
+import re
 from random import shuffle
 
 
@@ -39,6 +38,13 @@ class Score(object):
         self.multiplier = 1
         self.cross_score = 0
         self.board = board
+        
+    def copy(self):
+        dup = Score(self.board)
+        dup.score = self.score
+        dup.multiplier = self.multiplier
+        dup.cross_score = self.cross_score
+        return dup
     
     def play_tile(self, line, column, tile=None):
         field = self.board.board[line][column]
@@ -46,7 +52,9 @@ class Score(object):
             self.score += VALUES[field]
         else:
             tile_multiplier = 1
-            if field == ':':
+            if field == '.':
+                pass
+            elif field == ':':
                 tile_multiplier = 2
             elif field == '!':
                 tile_multiplier = 3
@@ -61,7 +69,6 @@ class Score(object):
     
     def get_word_score(self):
         return self.score * self.multiplier + self.cross_score
-
 
 
 class Play(object):
@@ -80,16 +87,19 @@ class Play(object):
                 yield letter if letter.isupper() else '*'
     
     def __repr__(self):
-        return "Play(%s,%s,%s,'%s',%s,'%s')" % (self.line, self.column, self.start, self.word, self.score, self.pattern)
+        return ("Play(%s,%s,%s,'%s',%s,'%s')" %
+                (self.line, self.column, self.start,
+                 self.word, self.score, self.pattern))
     
     def show(self):
         if self.column is None:
-            print "Across at line %d column %d play %s for %d points" % (self.line + 1, self.start + 1, self.word, self.score)
+            print ("Across at line %d column %d play %s for %d points" %
+                   (self.line + 1, self.start + 1, self.word, self.score))
         else:
-            print "  Down at line %d column %d play %s for %d points" % (self.start + 1, self.column + 1, self.word, self.score)
+            print ("  Down at line %d column %d play %s for %d points" %
+                   (self.start + 1, self.column + 1, self.word, self.score))
             
             
-
 class Board(object):
 
     def __init__(self, board=None, master=None):
@@ -97,18 +107,20 @@ class Board(object):
             self.board = board
         else:
             with open('board.txt') as data:
-                self.board = map(str.strip, data.readlines())
+                self.board = [line.strip() for line in data.readlines()]
         self.board_size = len(self.board)
         
         if master:
             self.transposed = master
-            self.board = [''.join(line[column] for line in self.board) for column in range(self.board_size)]
+            self.board = [''.join(line[column] for line in self.board)
+                          for column in range(self.board_size)]
         else:
             self.transposed = Board(board=self.board, master=self)
 
 
     def is_first_move(self):
-        return not any(any(map(str.isalpha, line)) for line in self.board)
+        return not any(any(char.isalpha() for char in line)
+                       for line in self.board)
 
     def get_pattern(self, line, start, length, first, num_letters):
 
@@ -133,7 +145,8 @@ class Board(object):
         # can't touch letters before or after
         if start > 0 and self.board[line][start - 1].isalpha():
             return None
-        if start + length < self.board_size and self.board[line][start + length].isalpha():
+        if (start + length < self.board_size and
+            self.board[line][start + length].isalpha()):
             return None
 
         # validate attaching to letter
@@ -141,9 +154,10 @@ class Board(object):
             return pattern
 
         # check if adjacent rows/columns have letters
-        if line and filter(str.isalpha, self.board[line - 1][start:start + length]):
-            return pattern
-        if line < self.board_size - 1 and filter(str.isalpha, self.board[line + 1][start:start + length]):
+        if ((line > 0 and
+             filter(str.isalpha, self.board[line - 1][start:start + length])) or
+            (line < self.board_size - 1 and
+             filter(str.isalpha, self.board[line + 1][start:start + length]))):
             return pattern
 
         return None
@@ -151,9 +165,11 @@ class Board(object):
 
     def get_playing_positions(self, num_letters):
         first = self.is_first_move()
-        for line, start in itertools.product(range(self.board_size), range(self.board_size - 1)):
+        for line, start in itertools.product(range(self.board_size),
+                                             range(self.board_size - 1)):
             for length in range(2, self.board_size - start):
-                pattern = self.get_pattern(line, start, length, first, num_letters)
+                pattern = self.get_pattern(line, start, length,
+                                           first, num_letters)
                 if pattern:
                     yield line, start, pattern
     
@@ -161,38 +177,50 @@ class Board(object):
     
     def check_cross_word(self, line, column, tile):
         from_column = to_column = column
-        score = Score(self)
         while from_column > 0 and self.board[line][from_column - 1].isalpha():
             from_column -= 1
-            score.play_tile(line, from_column)
-        while (to_column + 1 < self.board_size) and self.board[line][to_column + 1].isalpha():
+        while (to_column + 1 < self.board_size and
+               self.board[line][to_column + 1].isalpha()):
             to_column += 1
-            score.play_tile(line, to_column)
-        if from_column < to_column:
-            score.play_tile(line, column, tile)
-            check = self.board[line][from_column:column] + tile + self.board[line][column + 1:to_column + 1]
-            if not WORDS.get(check.upper()):
-                return -1
+        if from_column == to_column:
+            return 0
+        check = (self.board[line][from_column:column] + tile +
+                 self.board[line][column + 1:to_column + 1])
+        if not WORDS.get(check.upper()):
+            return -1
+        score = Score(self)
+        for col in range(from_column, column):
+            score.play_tile(line, col)
+        score.play_tile(line, column, tile)
+        for col in range(column + 1, to_column + 1):
+            score.play_tile(line, col)
         return score.get_word_score()
     
     
     def get_words(self, line, start, tiles, pattern):
-        used = len(filter(str.isalpha, pattern))
         found = dict()
+        invalid_cross_tiles = dict()
         
         def recurse(tiles, head, tail, score):
             for index, tile in enumerate(tiles):
-                options = tile if tile != '*' else filter(str.islower, VALUES.keys())
+                options = tile if tile != '*' else filter(str.islower,
+                                                          VALUES.keys())
                 for option in options:
+                    # check if we tried this letter before in this position
+                    if option in invalid_cross_tiles.setdefault(len(head), []):
+                        continue
+                    
                     word = head + option
-                    new_score = copy(score)
+                    new_score = score.copy()
                     new_score.play_tile(line, start + len(head), option)
                     
                     # check cross words
                     column = start + len(head)
-                    cross_score = self.transposed.check_cross_word(column, line, option)
+                    cross_score = self.transposed.check_cross_word(column, line,
+                                                                   option)
                     if cross_score < 0:
-                        # invalid cross word
+                        # invalid, remember letter to not try again in this pos
+                        invalid_cross_tiles[len(head)].append(option)
                         continue
                     else:
                         new_score.add_cross_score(cross_score)
@@ -212,7 +240,8 @@ class Board(object):
                         if check:
                             found[word] = new_score.get_word_score()
                     else:
-                        recurse(tiles[:index] + tiles[index + 1:], word, remaining, new_score)
+                        recurse(tiles[:index] + tiles[index + 1:], word,
+                                remaining, new_score)
 
         head = ''
         score = Score(self)
@@ -229,30 +258,30 @@ class Board(object):
         for line, start, pattern in self.get_playing_positions(len(tiles)):
             for word, score in self.get_words(line, start, tiles, pattern):
                 yield Play(line, None, start, word, score, pattern)
-        for line, start, pattern in self.transposed.get_playing_positions(len(tiles)):
-            for word, score in self.transposed.get_words(line, start, tiles, pattern):
+        for line, start, pattern in \
+            self.transposed.get_playing_positions(len(tiles)):
+            for word, score in \
+                self.transposed.get_words(line, start, tiles, pattern):
                 yield Play(None, line, start, word, score, pattern)
 
 
     def show(self):
-        print '\n'.join(self.board)
+        print re.sub('[.:!23]', ' ', '\n'.join(self.board))
 
 
     def play(self, play):     #line, column, start, word):
         if play.column is None:
             self.board[play.line] = (self.board[play.line][:play.start] +
-                                play.word +
-                                self.board[play.line][play.start + len(play.word):])
+                play.word +
+                self.board[play.line][play.start + len(play.word):])
         else:            
-            for index, line in enumerate(range(play.start, play.start + len(play.word))):
+            for index, line in enumerate(range(play.start, play.start +
+                                               len(play.word))):
                 self.board[line] = (self.board[line][:play.column] +
                                     play.word[index] +
                                     self.board[line][play.column + 1:])
         
         self.transposed = Board(board=self.board, master=self)
-
-
-
 
 
 class Player(object):
@@ -269,7 +298,8 @@ class Player(object):
             self.tiles += self.drawpile.pop()
     
     def play(self, board):
-        plays = sorted(board.get_plays(self.tiles), key=lambda play: play.score, reverse=True)
+        plays = sorted(board.get_plays(self.tiles),
+                       key=lambda play: play.score, reverse=True)
         if not plays:
             self.tiles = ''
         else:
@@ -282,77 +312,22 @@ class Player(object):
         
 
 def play_game():
-    
-    drawpile = copy(TILES)
-    
+    drawpile = TILES[:]
     board = Board()
     player1 = Player(drawpile)
     player2 = Player(drawpile)
-
     while True:    
         print "Player 1: %d %s" % (player1.score, player1.tiles)
         print "Player 2: %d %s" % (player2.score, player2.tiles)
-        board.show()
-        print "-" * 70
-    
         print "Remaining: ", ''.join(sorted(drawpile))
-       # raw_input('Enter to continue...')
-        
+        print "-" * 70
         if player1.tiles and player2.tiles:
             player1.play(board)
             player2.play(board)
         else:
             break
-        
+    board.show()
+    
 
 if __name__ == "__main__":
-
     play_game()
-    exit()
-
-    board = Board()
-    
-    #print board.is_first_move()
-    #
-    #board.show()
-    
-    #board.play(Play(None, 7, 4, 'HELLO'))
-    #board.play(Play(4, None, 7, 'HAT'))
-    
-    #print "\n"
-    #
-    #print board.is_first_move()
-    #
-    board.show()
-    #
-    #
-    #s = Score(board)
-    #s.play_tile(0, 2, 'a')
-    #s.play_tile(0, 3, 'E')
-    #print s.get_word_score()
-    #
-    #exit()
-
-    
-  #  start = datetime.datetime.now()
-    
-    plays = list(board.get_plays('HAMMEQZ'))
-    
- #   print datetime.datetime.now() - start
-
-    #for play in plays:
-    #    print play
-    print len(plays)
-    
-    plays = sorted(plays, key=lambda play: play.score, reverse=True)
-    for play in plays[:10]:
-        play.show()
-        print repr(play)
-        
-        
-    board.play(plays[0])
-    
-    board.show()
-    
-    exit()
-    
